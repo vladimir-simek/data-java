ca8b0c5ec439cd6cb7e52c812536804af8b2386b
vladimir-simek <minervladik@gmail.com>
2021-02-09 20:12:52 +0100

    sum of everything

--- src/com/vladimirsimek/datajava/Main.java
+++ src/com/vladimirsimek/datajava/Main.java
@@ -1,6 +1,11 @@
 package com.vladimirsimek.datajava;
 
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import static com.vladimirsimek.datajava.Logic.*;
 
 public class Main {
@@ -15,6 +20,21 @@ public class Main {
         readAndWriteVF("data/VF_2020.dat", "data-export/VF_2020.csv");
 
         writeFaktury(a);
+
+        double sum = 0;
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("data/sum.txt"));
+
+            for (int i = 0; i < 6; i++) {
+                String num = br.readLine();
+                System.out.println(num);
+                sum += Double.parseDouble(num);
+            }
+            System.out.println(sum);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
